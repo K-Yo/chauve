58a847fda5e1dfc7e512afffd5dddbca63ed4b93
Olivier CAILLAULT <olivier.caillault@ext.hermes.com>
2026-04-13 21:35:11 +0200

    dynamic settings

--- src/entities/settings.rs
+++ src/entities/settings.rs
@@ -1,6 +1,6 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct Settings {
     #[serde(default)]
     pub providers: ProvidersSettings,
@@ -9,13 +9,13 @@ pub struct Settings {
     pub poll_frequency: u64,
 }
 
-#[derive(Deserialize, Default, Clone, Debug)]
+#[derive(Deserialize, Serialize, Default, Clone, Debug)]
 pub struct ProvidersSettings {
     #[serde(default)]
     pub grafana: Vec<ProviderGrafanaSetting>,
 }
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct ProviderGrafanaSetting {
     pub url: String,
     pub token: String,
--- src/ui/header.rs
+++ src/ui/header.rs
@@ -3,16 +3,15 @@ use crate::entities::alert::Alert;
 use dioxus::prelude::*;
 
 #[component]
-fn LastUpdate(last_poll_time: String) -> Element {
+fn LastUpdate(last_poll_time: String, on_settings_toggle: EventHandler) -> Element {
     let mut poller_signal: Signal<Poller> = use_context();
 
     rsx! {
         "last poll time: {last_poll_time}"
 
-        div { class: "fixed top-0 right-0",
+        div { class: "fixed top-0 right-0 flex gap-1 p-1",
             button {
                 onclick: move |_| {
-                    // Same logic as above
                     let poller = poller_signal.read().clone();
                     spawn(async move {
                         if let Ok(data) = poller.poll_once().await {
@@ -24,6 +23,11 @@ fn LastUpdate(last_poll_time: String) -> Element {
                 id: "update",
                 class: "bg-orange-500 hover:bg-orange-700 text-black py-2 px-4 rounded",
                 "Update!"
+            }
+            button {
+                onclick: move |_| on_settings_toggle.call(()),
+                class: "bg-orange-500 hover:bg-orange-700 text-black py-2 px-4 rounded",
+                "⚙"
             }
         }
     }
@@ -42,18 +46,23 @@ pub fn AlertDetails(alert: Alert, unpin: EventHandler) -> Element {
 }
 
 #[component]
-pub fn Header(active_alert: Option<Alert>, is_pinned: bool,last_poll_time: String, unpin: EventHandler) -> Element {
-
-        rsx! {
-            div { class: "fixed top-0 right-0 left-0 overflow-y-scroll h-12 p-1",
-                match &active_alert {
-                    Some(alert) => rsx! {
-                        AlertDetails { alert: alert.clone(), unpin }
-                    },
-                    None => rsx! {
-                        LastUpdate { last_poll_time }
-                    },
-                }
+pub fn Header(
+    active_alert: Option<Alert>,
+    is_pinned: bool,
+    last_poll_time: String,
+    unpin: EventHandler,
+    on_settings_toggle: EventHandler,
+) -> Element {
+    rsx! {
+        div { class: "fixed top-0 right-0 left-0 overflow-y-scroll h-12 p-1",
+            match &active_alert {
+                Some(alert) => rsx! {
+                    AlertDetails { alert: alert.clone(), unpin }
+                },
+                None => rsx! {
+                    LastUpdate { last_poll_time, on_settings_toggle }
+                },
             }
         }
+    }
 }
